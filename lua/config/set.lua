b9a11a043fbcf93ea47e72ea40e688c2772af77a
scontt <deelog1233223@gmail.com>
2025-08-09 16:17:40 +0700

    Initial commit

---
+++ lua/config/set.lua
@@ -0,0 +1,10 @@
+vim.cmd.colorscheme = "melange"
+
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate",
+
+    config = function()
+
+        require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "go", "java", "javascript", "typescript" },
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+end
+}
